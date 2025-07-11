19c62a95605d127939a0de8b3bd6f00372c2fc5b
kimtaesung <kim25576@naver.com>
2025-07-11 18:32:00 +0900

    154 fail, make rebase

--- Hyper_FPS/Assets/Scripts/Weapon.cs
+++ Hyper_FPS/Assets/Scripts/Weapon.cs
@@ -2,23 +2,37 @@ using UnityEngine;
 using StarterAssets;
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] int damageAmount = 1;
+
     StarterAssetsInputs starterAssetsInputs;
 
-    private void Awake()
+    void Awake()
     {
         starterAssetsInputs = GetComponent<StarterAssetsInputs>();
     }
-    private void Update()
+    void Update()
     {
-        if(starterAssetsInputs.Shoot)
-        {
-            RaycastHit hit;
+        HandleShoot();
+    }
 
-            if(Physics.Raycast(transform.position,Camera.main.transform.forward, out hit, Mathf.Infinity)){
-                Debug.Log(hit.collider.name);
-                starterAssetsInputs.Shoot = false;
-            }
+    void HandleShoot()
+    {
+        if (!starterAssetsInputs.Shoot) return;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        {   
+            Debug.Log(hit.collider.name);  
+            /*
+            EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+            enemyHealth?.TakeDamage(damageAmount);
+            //if (enemyHealth != null)
+            //{
+            //    enemyHealth.TakeDamage(damageAmount);
+            //}
+            starterAssetsInputs.ShootInput(false);
+            */
         }
     }
 }
-
