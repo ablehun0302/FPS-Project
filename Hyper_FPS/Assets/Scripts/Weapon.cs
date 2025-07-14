6e7009df245a09e94a2170da79ddca04ece0cd90
ablehun0302 <ablehun@naver.com>
2025-07-14 22:51:34 +0900

    153. Shoot 인풋

--- Hyper_FPS/Assets/Scripts/Weapon.cs
+++ Hyper_FPS/Assets/Scripts/Weapon.cs
@@ -1,12 +1,30 @@
+using StarterAssets;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    StarterAssetsInputs starterAssetsInputs;
+
+    void Awake()
+    {
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+    }
+
     void Update()
     {
-        RaycastHit hit;
+        if (starterAssetsInputs.shoot)
+        {
+            RaycastHit hit;
 
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
-            Debug.Log(hit.collider.name);
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+            {
+                Debug.Log(hit.collider.name);
+            }
+            else
+            {
+                Debug.LogWarning("Null");
+            }
+        }
+        starterAssetsInputs.ShootInput(false);
     }
 }
